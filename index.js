439922c4fb3b11acab1addb23d2ed30650a9bba8
CodySt0 <Codistgeorge@gmail.com>
2024-06-24 10:37:42 -0700

    added more js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-async function getUsers() {
+const poke_container = document.getElementById("poke-container");
+// console.log(poke_container);
+const pokemon_count = 150;
+
+const fetchPokemons = async () => {
+  for (let id = 1; i <= pokemon_count; id++) {
+    //we are going to call a function that makes the API call
+    await getPokemon(id); // 1, 2, 3, 4, etc...
+  }
+};
+
+const getPokemon = async (id) => {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
+    //API call to the pokeAPI
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const pokemon = await response.json();
+    console.log(pokemon);
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-getUsers();
+fetchPokemons();
